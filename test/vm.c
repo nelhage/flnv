38bba39a0b5caa3c1cb6d9d8e33279dd44bc693f
Nelson Elhage <nelhage@mit.edu>
2009-01-27 02:30:13 -0500

    A simple test of creating and calling a function

--- test/vm.c
+++ test/vm.c
@@ -62,7 +62,6 @@ START_TEST(vm_arithmetic)
 {
     uint8_t buf[1024];
     uint8_t *code = buf;
-    int ops = 0;
     gc_handle v;
 
     vm_set_ip(buf);
@@ -71,38 +70,93 @@ START_TEST(vm_arithmetic)
     EMIT_INT(1024);
     vm_step_one();
 
-    EMIT_OP(OP_PUSH_INT); ops++;
+    EMIT_OP(OP_PUSH_INT);
     EMIT_INT(72);
     vm_step_one();
 
-    EMIT_OP(OP_ADD); ops++;
+    EMIT_OP(OP_ADD);
     vm_step_one();
 
     v = vm_top();
     fail_unless(sc_numberp(v));
     fail_unless(sc_number(v) == (1024+72));
 
-    EMIT_OP(OP_PUSH_INT); ops++;
+    EMIT_OP(OP_PUSH_INT);
     EMIT_INT(7777);
     vm_step_one();
 
-    EMIT_OP(OP_SUB); ops++;
+    EMIT_OP(OP_SUB);
     vm_step_one();
 
     v = vm_top();
     fail_unless(sc_numberp(v));
     fail_unless(sc_number(v) == (1024+72)-7777);
 
-    EMIT_OP(OP_PUSH_INT); ops++;
+    EMIT_OP(OP_PUSH_INT);
     EMIT_INT(1234);
     vm_step_one();
 
-    EMIT_OP(OP_MUL); ops++;
+    EMIT_OP(OP_MUL);
     vm_step_one();
 
     v = vm_top();
     fail_unless(sc_numberp(v));
     fail_unless(sc_number(v) == ((1024+72)-7777)*1234);
+}
+END_TEST
+
+#define OP(o)           EMIT_OP(OP_##o)
+#define I               EMIT_INT
+#define PI(i)           do{OP(PUSH_INT); I(i);} while(0)
+#define ALIGN_CODE      while((uint32_t)code % 4) {OP(NOP);}
+
+START_TEST(vm_closure)
+{
+    uint8_t buf[4096];
+    uint8_t *code = buf;
+    uint8_t *entry;
+    uint8_t *reloc;
+
+    /* Define a square function */
+    ALIGN_CODE;
+
+    reloc = code;
+    PI(0); PI(0); OP(ENV_REF);
+    OP(DUP);
+    OP(MUL);
+    OP(SWAP);
+    OP(JMP);
+
+    ALIGN_CODE;
+
+    entry = code;
+
+    PI(1);
+    OP(MAKE_CLOSURE);
+    I((reloc - (code + 4)));
+
+    PI(1);
+    OP(EXTEND_ENV);
+
+    OP(PUSH_ADDR);
+    reloc = code; I(0);
+
+    PI(32);
+    PI(0); PI(0); OP(ENV_REF);
+    PI(1);
+    OP(INVOKE_PROCEDURE);
+
+    ALIGN_CODE;
+
+    *(uint32_t*)reloc = (code - (reloc + 4));
+
+    OP(QUIT);
+
+    vm_set_ip(entry);
+    while(!vm_terminatedp()) vm_step_one();
+
+    fail_unless(sc_numberp(vm_top()));
+    fail_unless(sc_number(vm_top()) == 32*32);
 }
 END_TEST
 
@@ -115,6 +169,7 @@ Suite *vm_suite()
 
     tcase_add_test(tc, vm_load_int);
     tcase_add_test(tc, vm_arithmetic);
+    tcase_add_test(tc, vm_closure);
 
     suite_add_tcase(s, tc);
 
